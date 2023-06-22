026da11be5073a00949c2ba5f8028965ee323de6
greg <gregadgreg@htomail.com>
2023-06-22 08:40:40 +0200

    permission

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, Alert, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import MyButton from '../Components/MyButton';
 import NetInfo from "@react-native-community/netinfo";
+import * as Location from 'expo-location'
 
 export default class Home extends React.Component 
 {
@@ -10,10 +11,7 @@ export default class Home extends React.Component
         super(props);
         this.state = 
         {
-            connectionInfo: null,
-           
-            
-
+            connectionInfo: null,    
         };
     }
 
@@ -27,52 +25,58 @@ export default class Home extends React.Component
         this.props.navigation.navigate("Inscription")
     }
 
-    loadConnectionInfo = () => 
+    wifiButton = async () =>
     {
-        NetInfo.fetch().then(state => {
-            this.setState({
-                connectionInfo: state, 
-                 
-               
-            });
-           
-        });
+        // Request location permission
+        await Location.requestPermissionsAsync();
 
+        NetInfo.fetch("wifi").then(state => 
+            {
+            console.log("SSID", state.details.ssid);
+            console.log("Is connected?", state.isConnected);
+            this.setState(
+                {
+                    connectionInfo: state, 
+                }
+            );
+        });
     }
 
     render() 
     {
-        const { connectionInfo, ssidString, isConnected } = this.state;
+        const { connectionInfo } = this.state;
 
         return (
             <SafeAreaView style={styles.container}>
                 <Text>
                     ACCUEIL
                 </Text>
-                <View style={styles.buttonContainer}>
-                    <MyButton 
-                        onPress={this.connexionButton} 
-                        val="Connexion"
-                    />
-                    <MyButton 
-                        onPress={this.inscriptionButton} 
-                        val="Inscription"
-                    />
-                    <MyButton 
-                        onPress={this.loadConnectionInfo} 
-                        val="Afficher les informations de connexion"
-                    />
+                <MyButton 
+                    onPress={this.connexionButton} 
+                    val="Connexion"
+                />
+                <MyButton 
+                    onPress={this.inscriptionButton} 
+                    val="Inscription"
+                />
+                <MyButton 
+                    onPress={this.wifiButton} 
+                    val="WIFI"
+                />
+            {connectionInfo && (
+                <>
+                <View>
+                    <Text>Type de connexion : {connectionInfo.type}</Text>
+                    <Text>Connecté : {connectionInfo.isConnected ? 'Oui' : 'Non'}</Text>
+                    {
+                        connectionInfo.type === 'wifi' && 
+                        <Text>Nom du réseau (SSID) : {connectionInfo.details.ssid}</Text>
+                    }
                 </View>
-                {connectionInfo && (
-                    <>
-                        <View>
-                            <Text>Type de connexion : {connectionInfo.type}</Text>
-                            <Text>Connecté : {connectionInfo.isConnected ? 'Oui' : 'Non'}</Text>
-                            {connectionInfo.type === 'wifi' && <Text>Nom du réseau (SSID) : {connectionInfo.details.ssid}</Text>}
-                        </View>
                        
-                    </>
-                )}
+                </>
+                )
+            }    
             </SafeAreaView>
         );
     }
@@ -84,11 +88,5 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-    },
-    buttonContainer: {
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 20,
     }
 });
