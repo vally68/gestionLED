4581cec43c721f265badd267539265ce79bc2e13
Alex Iphigénial <74407411+SitrAlex68@users.noreply.github.com>
2023-06-26 13:23:31 +0200

    Test commit

--- Components/MyButton.js
+++ Components/MyButton.js
@@ -22,20 +22,18 @@ export default class MyButton extends React.Component
 
 
 const styles = StyleSheet.create({
-button: {
-    backgroundColor: '#615197',
-    padding: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-    marginBottom:10,
-    width:150,
-    borderWidth:3,
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
+    button: {
+        backgroundColor: '#615197',
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        marginBottom:10,
+        width:150,
+        borderWidth:3,
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
 });
-
-
--- Components/MySlider.js
+++ Components/MySlider.js
@@ -3,28 +3,28 @@ import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 
 import Slider from '@react-native-community/slider';
 
-const MySlider = () => 
+const MySlider = () =>
 {
-  const [sliderValue, setSliderValue] = useState(15);
+    const [sliderValue, setSliderValue] = useState(15);
 
-  return (
-    
-      <View>
+    return (
 
-        <Text style={{color: 'black'}}>Valeur du slider : {sliderValue}</Text>
+        <View>
 
-        <Slider
-          maximumValue={100}
-          minimumValue={0}
-          minimumTrackTintColor="#307ecc"
-          maximumTrackTintColor="#000000"
-          step={1}
-          value={sliderValue}
-          onValueChange={(sliderValue) => setSliderValue(sliderValue)}
-        />
-      </View>
-    
-  );
+            <Text style={{color: 'black'}}>Valeur du slider : {sliderValue}</Text>
+
+            <Slider
+                maximumValue={100}
+                minimumValue={0}
+                minimumTrackTintColor="#307ecc"
+                maximumTrackTintColor="#000000"
+                step={1}
+                value={sliderValue}
+                onValueChange={(sliderValue) => setSliderValue(sliderValue)}
+            />
+        </View>
+
+    );
 };
 
 
